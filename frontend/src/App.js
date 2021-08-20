707f586b66a150d6110b8cfb64602c3270c91654
bounderoll <bounderoll.23@gmail.com>
2021-08-21 02:46:53 +0400

    fix react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,22 @@
-import React, {useState} from "react"
-
-import {RegisterModal} from "./components/RegisterModal/RegisterModal"
-import {LoginModal} from "./components/LoginModal/LoginModal"
-import {Map} from "./components/Map/Map"
+import React from "react"
+import {Redirect, Route, Switch} from "react-router-dom"
 
 import './App.css'
-import {Redirect, Route, Switch} from "react-router-dom";
+import {HomePage} from "./pages/HomePage/HomePage"
+import {MapPage} from "./pages/MapPage/MapPage"
 
 
 const App = () => {
-    const user = ''
-    const [showModal, setShowModal] = useState(null)
-
-    const handleUserLogout = () => {
-        console.log(user)
-    }
+    console.log('qwe')
 
     return (
-        <>
-            <div className='app'>
-                <Switch>
-                    <Route path='/home' component={} />
-                    <Route path='/map' component={} />
-                    <Redirect from='/' to='/home' />
-                </Switch>
-            </div>
-
-            <Map />
-            {
-                <div className={'buttons__container'}>
-                    {user ? (<button className={'btn btn__logout'} onClick={handleUserLogout}>Log out</button>) : (
-                        <>
-                            <button onClick={() => setShowModal('login')} className='btn btn__login'>Log in</button>
-                            <button onClick={() => setShowModal('register')} className='btn bnt__register'>Register
-                            </button>
-                        </>
-                    )}
-                </div>
-            }
-            <RegisterModal isShow={showModal === 'register'} handleClose={() => setShowModal(false)}/>
-            <LoginModal isShow={showModal === 'login'} handleClose={() => setShowModal(false)}/>
-        </>
+        <div className='app'>
+            <Switch>
+                <Route path='/home' render={() => <HomePage />} />
+                <Route path='/map' render={() => <MapPage />} />
+                <Redirect from='/' to='/home'/>
+            </Switch>
+        </div>
     )
 }
 
