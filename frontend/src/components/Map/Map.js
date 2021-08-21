350c32f55b41381e7ebe8e5207e13398b6c5627b
Kirill <k.larionov.02@mail.ru>
2021-08-21 15:35:58 +0400

    Ничего

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -13,7 +13,7 @@ export class Map extends React.Component {
         const map = new mapboxgl.Map({
             container: this.mapWrapper,
             style: 'mapbox://styles/mapbox/streets-v10',
-            center: [-73.985664, 40.748514],
+            center: [58.52557, 31.27419],
             zoom: 12
         })
 
